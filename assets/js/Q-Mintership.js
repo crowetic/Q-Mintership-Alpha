59bd5cc7603eb3c2b33a185015c6ab5e9c01ab3d
crowetic <jason@crowetic.com>
2025-02-27 15:46:36 -0800

    Bump to v1.06.6b

--- assets/js/Q-Mintership.js
+++ assets/js/Q-Mintership.js
@@ -1,4 +1,4 @@
-const Q_MINTERSHIP_VERSION = "1.06.5"
+const Q_MINTERSHIP_VERSION = "1.06.6"
 
 const messageIdentifierPrefix = `mintership-forum-message`
 const messageAttachmentIdentifierPrefix = `mintership-forum-attachment`
